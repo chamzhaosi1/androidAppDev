dd8dfc240dc7375e0eec0527000141a5fb847ea3
chamzhaosi <chamzhaosi0808@e.newera.my>
2022-10-08 18:48:08 +0800

    Week 2 Tutorial - Fragment

---
+++ Week2_Tutorial/fragment/app/src/main/java/com/fragment/MainActivity.kt
@@ -0,0 +1,38 @@
+package com.fragment
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val firstFragment = FirstFragment()
+        val secondFragment = SecondFragment()
+        val btnFirstFragment : Button = findViewById(R.id.btnFragment1)
+        val btnSecondFragment : Button = findViewById(R.id.btnFragment2)
+
+        supportFragmentManager.beginTransaction().apply {
+            replace(R.id.flFragment, firstFragment)
+            commit()
+        }
+
+        btnFirstFragment.setOnClickListener{
+            supportFragmentManager.beginTransaction().apply {
+                replace(R.id.flFragment, firstFragment)
+                addToBackStack(null)
+                commit()
+            }
+        }
+
+        btnSecondFragment.setOnClickListener{
+            supportFragmentManager.beginTransaction().apply {
+                replace(R.id.flFragment, secondFragment)
+                addToBackStack(null)
+                commit()
+            }
+        }
+    }
+}
