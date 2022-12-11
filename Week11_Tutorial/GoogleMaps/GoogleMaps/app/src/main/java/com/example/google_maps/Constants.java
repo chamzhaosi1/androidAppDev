278f48a028f7a3fd9d051272805d3b1c4430494a
chamzhaosi <chamzhaosi0808@e.newera.my>
2022-12-11 22:20:01 +0800

    Week 11 Tutorial map api done

--- Week11_Tutorial/GoogleMaps/GoogleMaps/app/src/main/java/com/example/google_maps/Constants.java
+++ Week11_Tutorial/GoogleMaps/GoogleMaps/app/src/main/java/com/example/google_maps/Constants.java
@@ -3,4 +3,6 @@ package com.example.google_maps;
 public class Constants {
 
     public static final int ERROR_DIALOG_REQUEST = 9001;
+    public static final int PERMISSIONS_REQUEST_ENABLE_GPS = 9002;
+    public static final int PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION = 9003;
 }
